680ae6bd18852c1a842d567c4774cefeafcdf11e
Janani-Balasooriya <jananidev7@gmail.com>
2023-02-15 22:37:02 +0530

    todos edit done

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,11 @@
 import { PencilIcon, TrashIcon } from "@heroicons/react/outline";
+import { useState } from "react";
 import "./TodoItem.css";
 
 function TodoItem({ todo, index, todos, setTodos }) {
+  const [isEdit, setIsEdit] = useState(false);
+  const [editTodoName, setEditTodoName] = useState("");
+
   const handleDelete = () => {
     const newTodos = todos.filter((item) => {
       if (todo.id == item.id) {
@@ -14,14 +18,49 @@ function TodoItem({ todo, index, todos, setTodos }) {
     setTodos(newTodos);
   };
 
+  const handleEdit = () => {
+    setIsEdit(!isEdit);
+    setEditTodoName(todo.name);
+  };
+
+  const handleSubmit = (e) => {
+    if (e.keyCode == 13) {
+      console.log("in");
+      const newTodos = todos.map((item) => {
+        if (todo.id == item.id) {
+          return {
+            ...item,
+            name: editTodoName,
+          };
+        } else {
+          return item;
+        }
+      });
+      console.log(newTodos);
+      setTodos(newTodos);
+      setIsEdit(false);
+    }
+  };
+
   return (
     <div className="todo-item-wrapper">
       <div className="todo-item-text">
         <div>{index}.</div>
-        <div>{todo.name}</div>
+        {isEdit ? (
+          <input
+            type="text"
+            value={editTodoName}
+            onChange={(e) => {
+              setEditTodoName(e.target.value);
+            }}
+            onKeyDown={handleSubmit}
+          />
+        ) : (
+          <div>{todo.name}</div>
+        )}
       </div>
       <div className="todo-item-buttons">
-        <button className="pencil-button">
+        <button className="pencil-button" onClick={handleEdit}>
           <PencilIcon />
         </button>
         <button className="trash-button" onClick={handleDelete}>
